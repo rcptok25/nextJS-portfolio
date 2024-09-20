0361fe243e3f8e547c7ef40e0c3155b8a4ab5def
rtok <rtok@yukatech.com>
2024-09-21 01:37:24 +0300

    feat: initialize project with Next.js setup and basic layout structure

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist(nextConfig);
